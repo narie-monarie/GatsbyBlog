b723a0370d36aac0ecfbdd6edbf5cbfceb4c24ae
monarie <jobmonari7@gmail.com>
2023-01-11 16:07:35 +0300

    add in the about page correction 2

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,13 +7,13 @@ const about: React.FC = () => {
   return (
     <main>
       <Layout pageTitle="About Me">
-        <p>i code in: Java | Typescript | Go && C++ [for Data Structures]</p>
+        <hr />
+        <p>I code in: Java | Typescript | Go && C++ [for Data Structures]</p>
         <p>Currently learning ML in python</p>
         <h5>Contact me on</h5>
-        <hr />
         <p>Email: jobmonari7@gmail.com</p>
-        <p>WhatsApp: 0799435461</p>
-        <p>Github: 0799435461</p>
+        <p>WhatsApp: 0776379667</p>
+        <p>Github: <a href="github.com/narie-monarie">click here</a></p>
       </Layout>
     </main>
   );
